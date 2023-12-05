c17f2dafbf0d0a1741f21f64c2e87e8b2a734f27
4D Feline <tessercat@pakaa.net>
2023-12-05 09:24:31 -0700

    Root.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Parked domains
+
+This repo contains
+web resources for parked domains.
+
+See the
+[stack-deploy](https://github.com/tessercat/stack-deploy)
+repo for more info.
---
+++ php/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Welcome</title>
+    <link rel="apple-touch-icon" sizes="180x180" href="/img/a/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/img/a/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/img/a/favicon-16x16.png">
+    <link rel="manifest" href="/img/a/site.webmanifest">
+    <link rel="mask-icon" href="/img/a/safari-pinned-tab.svg" color="#1f2022">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="msapplication-config" content="/img/a/browserconfig.xml">
+    <meta name="theme-color" content="#1f2022">
+    <style>
+html {
+  font: normal normal normal 1.2rem/normal sans-serif;
+}
+body {
+  color: #ced4da;
+  background: #1f2022;
+}
+main {
+  margin: 3rem auto 0;
+}
+p {
+  text-align: center;
+}
+a {
+  font-size: x-large;
+  text-decoration: none;
+}
+a:link {
+  text-decoration: none;
+  color: white;
+}
+
+a:visited {
+  color: white;
+}
+    </style>
+  </head>
+  <body>
+    <main>
+      <p><a title="Mail the postmaster" href="mailto:postmaster@<?php echo $_SERVER['SERVER_NAME'] ?>">Hi</a></p>
+    </main>
+  </body>
+</html>
